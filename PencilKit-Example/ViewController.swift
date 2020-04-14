100435f1d74d463624e8f1b7251ab5ca57130150
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-04-14 18:43:49 -0300

    Done

--- PencilKit-Example/ViewController.swift
+++ PencilKit-Example/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import PencilKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate {
+
+    let canvasView = PKCanvasView(frame: UIScreen.main.bounds)
+    var toolPicker: PKToolPicker?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        view.addSubview(canvasView)
+        canvasView.delegate = self
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        guard let window = view.window else { return }
+        toolPicker = PKToolPicker.shared(for: window)
+        toolPicker?.setVisible(true, forFirstResponder: canvasView)
+        toolPicker?.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+    }
+
+    func canvasViewDidBeginUsingTool(_ canvasView: PKCanvasView) {
+        // Using it for trolling purposes :D
+        guard let toolPicker = toolPicker else { return }
+
+        if let inkingTool = toolPicker.selectedTool as? PKInkingTool {
+            canvasView.backgroundColor = inkingTool.color
+        }
+    }
 }
 
